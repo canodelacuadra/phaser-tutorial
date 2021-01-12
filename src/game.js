30b2ce4fc896730701598a5f4cd5d197b254f988
canodelacuadra <canodelacuadra@gmail.com>
2021-01-12 19:10:36 +0100

    colisiones y rebotes

--- src/game.js
+++ src/game.js
@@ -8,15 +8,28 @@ export class Game extends Phaser.Scene {
     this.load.image('background', 'assets/background.png');
       this.load.image('gameover', 'assets/gameover.png');
         this.load.image('platform', 'assets/platform.png');
+        this.load.image('ball', 'assets/ball.png');
   }
 
   create() {
     this.add.image(400, 250, 'background');
       this.gameoverImage = this.add.image(400, 90, 'gameover');
       this.gameoverImage.visible = false;
-      this.platform = this.physics.add.image(400, 460, 'platform');
+      this.platform = this.physics.add.image(400, 460, 'platform').setImmovable();
       this.platform.body.allowGravity = false;
       this.cursors = this.input.keyboard.createCursorKeys();
+      this.ball = this.physics.add.image(400, 30, 'ball');
+      this.ball.setCollideWorldBounds(true);
+      this.ball.setBounce(1);
+      let velocity = 100 * Phaser.Math.Between(1.3, 2);
+        if (Phaser.Math.Between(0, 10) > 5) {
+  velocity = 0 - velocity;
+        }
+    this.ball.setVelocity(velocity, 10);   
+    this.physics.world.setBoundsCollision(true, true, true, false); 
+      this.physics.add.collider(this.ball, this.platform);
+      
+ 
   }
   update() {
     if (this.cursors.left.isDown) {
@@ -27,7 +40,13 @@ export class Game extends Phaser.Scene {
     }
     else {
       this.platform.setVelocityX(0);
-    }
+      }
+   // pantalla fin de juego
+      if (this.ball.y > 500) {
+      console.log('fin');
+          this.gameoverImage.visible = true;
+           this.scene.pause();
+    }   
   }
 
 }
