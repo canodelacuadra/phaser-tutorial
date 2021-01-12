401a991978a37523e58e6f065ccf542044f3299b
canodelacuadra <canodelacuadra@gmail.com>
2021-01-12 09:40:27 +0100

    subir imagenes y fisicas

--- src/init.js
+++ src/init.js
@@ -1,21 +1,34 @@
 const config = {
-    width: 320,
-    height: 180,
+    width: 320*2,
+    height: 180*2,
     parent:"container",
     type: Phaser.AUTO,
     scene:{
         preload:preload,
         create:create,
         update:update
+    },
+    physics:{
+        default:"arcade",
+        arcade:{
+            gravity:{
+                y:500
+            }
+        }
     }
 
 }
 var game = new Phaser.Game(config)
+// se encarga de precargar assets
 function preload(){
-    console.log('soy preload');
+    this.load.image("bird","./assets/bird.png")
+  
 }
 function create(){
-    console.log('soy create');
+    this.bird = this.physics.add.image(50,100, "bird");
+   this.bird.setCollideWorldBounds(true);
+   this.bird.setBounce(0.2);
+   this.bird.setVelocity(50,0)
 }
 function update(time,delta){
     console.log('soy update');
